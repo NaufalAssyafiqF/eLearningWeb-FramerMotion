dff9725cffdf4aba409972ff3b81426b9ce849e6
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-09-08 22:34:50 +0700

    add hero section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { IoMdMenu } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const NavbarMenu = [
   {
@@ -30,8 +31,12 @@ const NavbarMenu = [
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container py-10 flex justify-between items-center">
+    <nav className="relative z-20">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="container py-10 flex justify-between items-center"
+      >
         <div>
           <h1 className="font-bold text-2xl">The Coding Journey</h1>
         </div>
@@ -39,16 +44,22 @@ const Navbar = () => {
           <ul className="flex items-center gap-3">
             {NavbarMenu.map((menu) => (
               <li key={menu.id}>
-                <a href={menu.path} className="inline-block py-2 px-3 hover:text-secondary relative group"><div className="w-2 h-2 bg-secondary absolute mt-2 left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden rounded-full"></div>{menu.title}</a>
+                <a
+                  href={menu.path}
+                  className="inline-block py-2 px-3 hover:text-secondary relative group"
+                >
+                  <div className="w-2 h-2 bg-secondary absolute mt-2 left-1/2 -translate-x-1/2 top-1/2 bottom-0 group-hover:block hidden rounded-full"></div>
+                  {menu.title}
+                </a>
               </li>
             ))}
             <button className="primary-btn">Sign In</button>
           </ul>
         </div>
         <div className="lg:hidden">
-          <IoMdMenu className="text-4xl"/>
+          <IoMdMenu className="text-4xl" />
         </div>
-      </div>
+      </motion.div>
     </nav>
   );
 };
