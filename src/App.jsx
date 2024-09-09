cefcda8b72a2d8eb57d199a991af71bcb73e5861
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-09-09 21:43:30 +0700

    add banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner/Banner";
 import Hero from "./components/Hero/Hero";
 import Service from "./components/ServiceSection/Service";
 
@@ -6,6 +7,7 @@ const App = () => {
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
       <Service />
+      <Banner />
     </main>
   );
 };
