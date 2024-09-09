981eeae665f235f61c5c99100dd2a510e8eee367
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-09-09 21:14:42 +0700

    add service section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero";
+import Service from "./components/ServiceSection/Service";
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
+      <Service />
     </main>
   );
 };
