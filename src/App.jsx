13ad733324578bfd0c65144479ae399c04faf054
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2024-09-19 09:56:57 +0700

    add subscribe, join n footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Banner from "./components/Banner/Banner";
+import Banner2 from "./components/Banner/Banner2";
+import FooterSection from "./components/Footer/FooterSection";
 import Hero from "./components/Hero/Hero";
 import Service from "./components/ServiceSection/Service";
+import Subscribe from "./components/Subscribe/Subscribe";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Hero />
       <Service />
       <Banner />
+      <Subscribe />
+      <Banner2 />
+      <FooterSection />
     </main>
   );
 };
